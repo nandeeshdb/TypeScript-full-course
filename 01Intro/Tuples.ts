9825b3fce7880fd2962cfe35a4947c9ab711f0f5
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2023-12-13 16:47:37 +0530

    Tuples

---
+++ 01Intro/Tuples.ts
@@ -0,0 +1,10 @@
+let User4:[number,string] 
+User4=[1,"A"]
+
+// tuples in typescipt is used to make array more rescrtion , about its lenth and about its order of items
+
+let rgb:[number,number,number] = [1,2,3]
+
+type User5 = [number,string]
+
+const newUser:User5=[1,'a']
