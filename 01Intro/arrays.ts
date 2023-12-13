5223b67a4619d514a9e660d438bb12aa54b8526d
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2023-12-13 15:29:26 +0530

    Arrays

---
+++ 01Intro/arrays.ts
@@ -0,0 +1,29 @@
+const HeroName:string[] = []
+const HeroPower:number[]=[]
+{/*
+even we can define like this 
+
+const HeroName : Array<string>=[]
+const HeroPower:Array<number>=[]
+ */}
+
+
+HeroName.push("hulk")
+HeroPower.push(1)
+
+
+type User ={
+    name:string
+    mobile:number
+}
+
+const CreateUser:User[]=[]
+CreateUser.push({name:"",mobile:1})
+
+
+//an array inside array
+const HeroineNumber : number[][]=[
+    [111]
+]
+HeroineNumber.push([222])
+
